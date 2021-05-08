651aebf73ed767806bc05b41a86c3821faa30494
mo-sheikh <mohamed-sheikh1@outlook.com>
2021-05-08 20:30:51 +0100

    implementing sell coin

--- execute_order.py
+++ execute_order.py
@@ -2,6 +2,9 @@ import json
 from datetime import datetime
 
 
+
+
+
 class execute_order:
 
     def __init__(self):
@@ -26,7 +29,7 @@ class execute_order:
         obj = {
             "Name": coin['name'],
             "price": coin["current_price"],
-            "date": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+            "date": datetime.utcnow().isoformat(),
             "1hr_price_change_at_purchase": coin['price_change_percentage_1h_in_currency'],
             "ath_change_percentage_at_purchase": coin['ath_change_percentage'],
             "status": "executed"
@@ -38,6 +41,16 @@ class execute_order:
             json.dump(self.orders, f_obj)
 
         return 1
+
+    def is_coin_over_48_hours_old(self, coin):
+        difference = (datetime.utcnow() - coin['date']).total_seconds()
+        two_days_in_seconds = 172800
+        return difference >= two_days_in_seconds
+    def sell_coins(self):
+        coin_api = "https://api.coingecko.com/api/v3/coins/%s"
+        for coin in self.orders:
+            if self.is_coin_over_48_hours_old(coin) or self.has_coin_reached_threshold(coin) or self.is_equal_or_lower_than_initial_cost(coin):
+
 
 
 if __name__ == "__main__":

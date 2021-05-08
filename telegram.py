49518b4bb8abafdaf9a61866f64a3be5c3792591
Senna Semakula-Buuza <senna.semakula@outlook.com>
2021-05-08 13:32:11 +0100

    add telegram script

---
+++ telegram.py
@@ -0,0 +1,15 @@
+import requests
+
+def telegram_bot_sendtext(bot_message):
+    
+    bot_token = '1866321485:AAGUYPjFZFHNdMII-tMAoG5xPej1jBK_b1M'
+    bot_chatID = '1760050070'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
+    
+
+# TODO: Mo to loop through results and send 5 tokens to chatg
+telegram_bot_sendtext("Hello Senna from bot!")

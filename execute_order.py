af3148040df904682ec93897714a0d2be8ef595f
mo-sheikh <mohamed-sheikh1@outlook.com>
2021-05-08 19:21:26 +0100

    execute order implemented

--- execute_order.py
+++ execute_order.py
@@ -2,11 +2,10 @@ import json
 from datetime import datetime
 
 
-
 class execute_order:
 
     def __init__(self):
-        with open ("../results.json") as results,  open("../orders.json") as orders:
+        with open("results.json") as results, open("orders.json") as orders:
             self.coins = json.load(results)
             self.orders = json.load(orders)
             self.iter = iter(self.coins)
@@ -18,10 +17,11 @@ class execute_order:
             print(e)
 
     def buy_coin(self):
-        coin = next(self.iter)
+        coin = next(self.iter, None)
+        if not coin: return None
         if self.is_coin_already_purchased(coin):
             print(f"{coin['name']} already purchased")
-            return
+            return 0
 
         obj = {
             "Name": coin['name'],
@@ -32,9 +32,12 @@ class execute_order:
             "status": "executed"
         }
         self.orders.append(obj)
-
-        with open('../orders.json', 'w+') as f_obj:
+        print(f"purchasing {coin['name']}")
+        print(self.orders)
+        with open('orders.json', 'w+') as f_obj:
             json.dump(self.orders, f_obj)
+
+        return 1
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+from execute_order import execute_order
 from fetch_crypto import request, sort_results, save_results, log_results
 
 if __name__ == "__main__":
 
-    results = request()
-    key_price_change = "price_change_percentage_1h_in_currency"
-    key_ath_percentage = "ath_change_percentage"
-    key_market_cap = "market_cap"
-    key_current_price = "current_price"
-    sorted_res = sort_results(results, key_price_change, key_ath_percentage, key_market_cap)
-    save_results(sorted_res[:10])
-    log_results()
+    # results = request()
+    # key_price_change = "price_change_percentage_1h_in_currency"
+    # key_ath_percentage = "ath_change_percentage"
+    # key_market_cap = "market_cap"
+    # key_current_price = "current_price"
+    # sorted_res = sort_results(results, key_price_change, key_ath_percentage, key_market_cap)
+    # save_results(sorted_res[:10])
+    # log_results()
 
-    #Buying coin
+    # Buying coin
+    counter = 0
+    buyer = execute_order()
+    while (counter < 5):
+        print("here")
+        status = buyer.buy_coin()
+        if status or status == 0:
+            counter += status
+            continue
+        else:
+            print("breaking")
+            break
+
